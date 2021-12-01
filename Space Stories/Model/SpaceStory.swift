f0703e0d5d57e80b47dbdbf9b89bde34ee2c5a29
AlexeyBH <alexey_bh@mail.ru>
2021-12-01 23:11:47 +0300

    All finished

---
+++ Space Stories/Model/SpaceStory.swift
@@ -0,0 +1,16 @@
+//
+//  SpaceStory.swift
+//  Space Stories
+//
+//  Created by Alexey Khestanov on 01.12.2021.
+//
+
+import Foundation
+
+struct SpaceStory: Codable {
+    let explanation: String?
+    let url: String?
+    let hdurl: String?
+    let title: String?
+}
+
